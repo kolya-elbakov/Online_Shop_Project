6134a0f2eda6502178ad3b1c74b72123fa934cbd
Kolya Elbakov <mr.elbakov@mail.ru>
2023-12-17 17:18:15 +0800

    Login

---
+++ app/public/post_login.php
@@ -0,0 +1,34 @@
+<?php
+
+//print_r($_POST);
+
+$errors = [];
+
+$email = $_POST['email'];
+if(empty($email)){
+    $errors['email'] = 'Введите email';
+}
+if()
+
+$password = $_POST['password'];
+if(empty($password)) {
+    $errors['password'] = 'Введите пароль';
+}
+
+if(empty($errors)) {
+    $pdo = new PDO("pgsql:host=db;port=5432;dbname=postgres;", "dbuser", "dbpwd");
+
+    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $statement->execute(['email' => $email]);
+    $res = $statement->fetch();
+
+    if(password_verify($password, $res['password'])) {
+        session_start();
+        $_SESSION['user_id'] = $res['id'];
+        header("Location: /main.php");
+    } else {
+        $errors['password'] = 'Неверный пароль';
+    }
+}
+
+require_once './get_login.php';
